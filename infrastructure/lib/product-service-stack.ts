ce153294a5662639d3471786820058bbf40098e6
Ruslan Rabadanov <ruslanrabadanov2101@gmail.com>
2026-05-09 21:06:12 +0200

    task-4 Integration with DynamoDB

--- infrastructure/lib/product-service-stack.ts
+++ infrastructure/lib/product-service-stack.ts
@@ -2,6 +2,7 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as apigateway from 'aws-cdk-lib/aws-apigateway';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
 import * as path from 'path';
 
 export class ProductServiceStack extends cdk.Stack {
@@ -12,6 +13,9 @@ export class ProductServiceStack extends cdk.Stack {
 
     const backendPath = path.resolve(process.cwd(), '../../nodejs-aws-shop-backend/dist');
 
+    const productsTableName = process.env.PRODUCTS_TABLE_NAME || 'RSS_Products';
+    const stocksTableName = process.env.STOCKS_TABLE_NAME || 'RSS_Stocks';
+
     const getProductsListFn = new lambda.Function(this, 'GetProductsListFunction', {
       runtime: lambda.Runtime.NODEJS_24_X,
       handler: 'handlers/getProductsList.handler',
@@ -20,6 +24,8 @@ export class ProductServiceStack extends cdk.Stack {
       timeout: cdk.Duration.seconds(10),
       environment: {
         ALLOWED_ORIGIN: process.env.ALLOWED_ORIGIN || '*',
+        PRODUCTS_TABLE: productsTableName,
+        STOCKS_TABLE: stocksTableName,
       },
     });
 
@@ -31,8 +37,34 @@ export class ProductServiceStack extends cdk.Stack {
       timeout: cdk.Duration.seconds(10),
       environment: {
         ALLOWED_ORIGIN: process.env.ALLOWED_ORIGIN || '*',
+        PRODUCTS_TABLE: productsTableName,
+        STOCKS_TABLE: stocksTableName,
       },
     });
+
+    const productsTable = dynamodb.Table.fromTableName(this, 'ProductsTable', productsTableName);
+    const stocksTable = dynamodb.Table.fromTableName(this, 'StocksTable', stocksTableName);
+
+    productsTable.grantReadData(getProductsListFn);
+    stocksTable.grantReadData(getProductsListFn);
+    productsTable.grantReadData(getProductByIdFn);
+    stocksTable.grantReadData(getProductByIdFn);
+
+    const createProductFn = new lambda.Function(this, 'CreateProductFunction', {
+      runtime: lambda.Runtime.NODEJS_24_X,
+      handler: 'handlers/createProduct.handler',
+      code: lambda.Code.fromAsset(backendPath),
+      memorySize: 128,
+      timeout: cdk.Duration.seconds(10),
+      environment: {
+        ALLOWED_ORIGIN: process.env.ALLOWED_ORIGIN || '*',
+        PRODUCTS_TABLE: productsTableName,
+        STOCKS_TABLE: stocksTableName,
+      },
+    });
+
+    productsTable.grantWriteData(createProductFn);
+    stocksTable.grantWriteData(createProductFn);
 
     this.restApi = new apigateway.RestApi(this, 'ProductApi', {
       restApiName: 'Product Service API',
@@ -62,6 +94,8 @@ export class ProductServiceStack extends cdk.Stack {
     const products = this.restApi.root.addResource('products');
     const productsIntegration = new apigateway.LambdaIntegration(getProductsListFn);
     products.addMethod('GET', productsIntegration);
+    const createIntegration = new apigateway.LambdaIntegration(createProductFn);
+    products.addMethod('POST', createIntegration);
 
     const single = products.addResource('{productId}');
     const singleIntegration = new apigateway.LambdaIntegration(getProductByIdFn);
