e4bd278a21d292261929829423fdddb2f60a0590
Ruslan Rabadanov <ruslanrabadanov2101@gmail.com>
2026-06-01 21:56:22 +0200

    task-7 fix: omit auth token for empty localStorage; fix path to dist/

--- src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
+++ src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
@@ -12,6 +12,8 @@ export default function CSVFileImport({ url, title }: CSVFileImportProps) {
   const [file, setFile] = React.useState<File>();
   const [isUploading, setIsUploading] = React.useState(false);
 
+  const authorizationToken = localStorage.getItem("authorization_token");
+
   const onFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const files = e.target.files;
     if (files && files.length > 0) {
@@ -36,12 +38,14 @@ export default function CSVFileImport({ url, title }: CSVFileImportProps) {
         params: {
           name: file.name,
         },
-        headers: {
-          Authorization: `Basic ${localStorage.getItem('authorization_token')}`,
-        },
+        headers: authorizationToken
+          ? {
+              Authorization: `Basic ${authorizationToken}`,
+            }
+          : undefined,
       });
 
-      const uploadResponse = await fetch(response.data, {
+      const uploadResponse = await fetch(response.data as string, {
         method: "PUT",
         body: file,
         headers: {
